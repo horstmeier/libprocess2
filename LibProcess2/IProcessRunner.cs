2f212e203fedf5e49cab41973dab7f08d0bcaa21
Jens Horstmeier <jens@horstmeier.de>
2022-07-30 10:24:49 +0200

    Added RunLogged

---
+++ LibProcess2/IProcessRunner.cs
@@ -0,0 +1,14 @@
+﻿namespace LibProcess2;
+
+public interface IProcessRunner
+{
+    Task<int> Run(
+        string fileName,
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        Action<string?>? onOut = null,
+        Action<string?>? onErr = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    );
+}
---
+++ LibProcess2/ProcessRunnerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LibProcess2;
+
+public static class ProcessRunnerExtensions
+{
+    public static async Task<int> RunLogged(
+        this IProcessRunner processRunner,
+        ILogger log,
+        string logPrefix,
+        string fileName,
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null)
+    {
+        return await processRunner.Run(
+            fileName,
+            arguments,
+            workingDirectory,
+            s => log.LogDebug("{Prefix} {Message}", logPrefix, s),
+            s => log.LogDebug("{Prefix} E {Message}", logPrefix, s),
+            cancellationToken,
+            isSuccess);
+    }
+}
