02bf69449aaa91949c775d5ba808a3589945d302
Jens Horstmeier <jens@horstmeier.de>
2022-07-30 16:48:11 +0200

    Added string functions. closes #12

--- LibProcess2/ProcessRunnerExtensions.cs
+++ LibProcess2/ProcessRunnerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
 
 namespace LibProcess2;
 
@@ -22,5 +23,29 @@ public static class ProcessRunnerExtensions
             s => log.LogDebug("{Prefix} E {Message}", logPrefix, s),
             cancellationToken,
             isSuccess);
+    }
+
+    public static async Task<(int exitCode, string stdOut, string stdErr)> RunWithResult(
+        this IProcessRunner processRunner,
+        string fileName,
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    )
+    {
+        var sbOut = new StringBuilder();
+        var sbErr = new StringBuilder();
+
+        var exitCode = await processRunner.Run(
+            fileName,
+            arguments,
+            workingDirectory,
+            s => sbOut.AppendLine(s),
+            s => sbErr.AppendLine(s),
+            cancellationToken,
+            isSuccess);
+
+        return (exitCode, sbOut.ToString(), sbErr.ToString());
     }
 }
