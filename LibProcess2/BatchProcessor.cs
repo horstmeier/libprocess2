dc1e7ee9c63f8807a2b92e1a0cc7e41c95cbaed7
Jens Horstmeier <jens@horstmeier.de>
2022-07-30 19:08:41 +0200

    Added class BatchFile, closes #16

---
+++ LibProcess2/BatchProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Immutable;
+
+namespace LibProcess2;
+
+public class BatchProcessor
+{
+    private readonly IProcessRunner _processRunner;
+    private readonly string _comspec;
+
+    public BatchProcessor(IProcessRunner processRunner)
+    {
+        _comspec = Environment.GetEnvironmentVariable("comspec")
+                   ?? throw new Exception("Batch processor is not supported here");
+        _processRunner = processRunner;
+    }
+    public async Task<(int exitCode, string stdOut, string stdErr)> Run(
+        FileInfo batchFile,
+        IEnumerable<string> arguments,
+        CancellationToken? cancellationToken = null) =>
+        await _processRunner.RunWithResult(
+            _comspec,
+            arguments.Aggregate(ImmutableList<string>.Empty.Add("/c"),
+                (acc, it) => acc.Add(it)),
+            batchFile.DirectoryName,
+            cancellationToken,
+            null);
+}
---
+++ LibProcess2/ExternalApplicationEx.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System;
+namespace LibProcess2;
+
+public class ExternalApplicationEx : IExternalApplication
+{
+    private readonly IProcessRunner _processRunner;
+    private readonly string _fileName;
+    private readonly Func<IEnumerable<string>, IEnumerable<string>>? _argumentBuilder;
+    private readonly string? _defaultWorkingDirectory;
+    private readonly Action<string?>? _onOut;
+    private readonly Action<string?>? _onErr;
+    private readonly Func<int, bool>? _defaultIsSuccess;
+
+    public ExternalApplicationEx(
+        IProcessRunner processRunner,
+        string fileName,
+        Func<IEnumerable<string>, IEnumerable<string>>? argumentBuilder = null,
+        string? defaultWorkingDirectory = null,
+        Action<string?>? onOut = null,
+        Action<string?>? onErr = null,
+        Func<int, bool>? defaultIsSuccess = null)
+    {
+        _processRunner = processRunner;
+        _fileName = fileName;
+        _argumentBuilder = argumentBuilder;
+        _defaultWorkingDirectory = defaultWorkingDirectory;
+        _onOut = onOut;
+        _onErr = onErr;
+        _defaultIsSuccess = defaultIsSuccess;
+    }
+
+    public async Task<int> Run(
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    ) =>
+        await _processRunner.Run(
+            _fileName,
+            _argumentBuilder?.Invoke(arguments) ?? arguments,
+            workingDirectory ?? _defaultWorkingDirectory,
+            _onOut,
+            _onErr,
+            cancellationToken,
+            isSuccess ?? _defaultIsSuccess);
+
+    public async Task<(int exitCode, string stdOut, string stdErr)> RunWithResult(
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    )
+    {
+        var sbOut = new StringBuilder();
+        var sbErr = new StringBuilder();
+
+        var exitCode = await _processRunner.Run(
+            _fileName,
+            arguments,
+            workingDirectory ?? _defaultWorkingDirectory,
+            s =>
+            {
+                _onOut?.Invoke(s);
+                sbOut.AppendLine(s);
+            },
+            s =>
+            {
+                _onErr?.Invoke(s);
+                sbErr.AppendLine(s);
+            },
+            cancellationToken,
+            isSuccess ?? _defaultIsSuccess);
+        return (exitCode, sbOut.ToString(), sbErr.ToString());
+    }
+}
