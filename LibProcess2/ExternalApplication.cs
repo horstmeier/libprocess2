72fa6276c330e4d9995324358db5c1f58d4bc5af
Jens Horstmeier <jens@horstmeier.de>
2022-07-30 14:11:19 +0200

    Added ProcessRunnerEx and ExternalApplication closes #4

---
+++ LibProcess2/ExternalApplication.cs
@@ -0,0 +1,45 @@
+﻿namespace LibProcess2;
+
+public class ExternalApplication
+{
+    private readonly IProcessRunner _processRunner;
+    private readonly string _fileName;
+    private readonly string? _defaultWorkingDirectory;
+    private readonly Action<string?>? _onOut;
+    private readonly Action<string?>? _onErr;
+    private readonly Func<int, bool>? _defaultIsSuccess;
+
+    public ExternalApplication(
+        IProcessRunner processRunner,
+        string fileName,
+        string? defaultWorkingDirectory = null,
+        Action<string?>? onOut = null,
+        Action<string?>? onErr = null,
+        Func<int, bool>? defaultIsSuccess = null)
+    {
+        _processRunner = processRunner;
+        _fileName = fileName;
+        _defaultWorkingDirectory = defaultWorkingDirectory;
+        _onOut = onOut;
+        _onErr = onErr;
+        _defaultIsSuccess = defaultIsSuccess;
+    }
+
+    public async Task<int> Run(
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    )
+    {
+        return await _processRunner.Run(
+            _fileName,
+            arguments,
+            workingDirectory ?? _defaultWorkingDirectory,
+            _onOut,
+            _onErr,
+            cancellationToken,
+            isSuccess ?? _defaultIsSuccess);
+    }
+
+}
---
+++ LibProcess2/ProcessRunnerEx.cs
@@ -0,0 +1,30 @@
+﻿namespace LibProcess2;
+
+public class ProcessRunnerEx
+{
+    private readonly IProcessRunner _processRunner;
+    private readonly Action<string?>? _onOut;
+    private readonly Action<string?>? _onErr;
+
+    public ProcessRunnerEx(
+        IProcessRunner processRunner,
+        Action<string?>? onOut = null,
+        Action<string?>? onErr = null)
+    {
+        _processRunner = processRunner;
+        _onOut = onOut;
+        _onErr = onErr;
+    }
+
+    public async Task<int> Run(
+        string fileName,
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    )
+    {
+        return await _processRunner.Run(fileName, arguments, workingDirectory, _onOut, _onErr,
+            cancellationToken, isSuccess);
+    }
+}
