f4c8d898faa549805438c7ffa585648a47b0f4cc
Jens Horstmeier <jens@horstmeier.de>
2022-07-30 19:08:41 +0200

    Added class BatchFile, closes #16

--- LibProcess2/ExternalApplication.cs
+++ LibProcess2/ExternalApplication.cs
@@ -1,15 +1,8 @@
-﻿using System.Text;
+﻿namespace LibProcess2;
 
-namespace LibProcess2;
-
-public class ExternalApplication
+public class ExternalApplication : IExternalApplication
 {
-    private readonly IProcessRunner _processRunner;
-    private readonly string _fileName;
-    private readonly string? _defaultWorkingDirectory;
-    private readonly Action<string?>? _onOut;
-    private readonly Action<string?>? _onErr;
-    private readonly Func<int, bool>? _defaultIsSuccess;
+    private readonly ExternalApplicationEx _externalApplication;
 
     public ExternalApplication(
         IProcessRunner processRunner,
@@ -19,12 +12,14 @@ public class ExternalApplication
         Action<string?>? onErr = null,
         Func<int, bool>? defaultIsSuccess = null)
     {
-        _processRunner = processRunner;
-        _fileName = fileName;
-        _defaultWorkingDirectory = defaultWorkingDirectory;
-        _onOut = onOut;
-        _onErr = onErr;
-        _defaultIsSuccess = defaultIsSuccess;
+        _externalApplication = new ExternalApplicationEx(
+            processRunner,
+            fileName,
+            x => x,
+            defaultWorkingDirectory,
+            onOut,
+            onErr,
+            defaultIsSuccess);
     }
 
     public async Task<int> Run(
@@ -32,42 +27,9 @@ public class ExternalApplication
         string? workingDirectory = null,
         CancellationToken? cancellationToken = null,
         Func<int, bool>? isSuccess = null
-    ) =>
-        await _processRunner.Run(
-            _fileName,
-            arguments,
-            workingDirectory ?? _defaultWorkingDirectory,
-            _onOut,
-            _onErr,
-            cancellationToken,
-            isSuccess ?? _defaultIsSuccess);
+    ) => await _externalApplication.Run(arguments, workingDirectory, cancellationToken, isSuccess);
 
-    public async Task<(int exitCode, string stdOut, string stdErr)> RunWithResult(
-        IEnumerable<string> arguments,
-        string? workingDirectory = null,
-        CancellationToken? cancellationToken = null,
-        Func<int, bool>? isSuccess = null
-    )
-    {
-        var sbOut = new StringBuilder();
-        var sbErr = new StringBuilder();
-
-        var exitCode = await _processRunner.Run(
-            _fileName,
-            arguments,
-            workingDirectory ?? _defaultWorkingDirectory,
-            s =>
-            {
-                _onOut?.Invoke(s);
-                sbOut.AppendLine(s);
-            },
-            s =>
-            {
-                _onErr?.Invoke(s);
-                sbErr.AppendLine(s);
-            },
-            cancellationToken,
-            isSuccess ?? _defaultIsSuccess);
-        return (exitCode, sbOut.ToString(), sbErr.ToString());
-    }
+    public async Task<(int exitCode, string stdOut, string stdErr)> RunWithResult(IEnumerable<string> arguments, string? workingDirectory = null, CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null) =>
+        await _externalApplication.RunWithResult(arguments, workingDirectory, cancellationToken, isSuccess);
 }
---
+++ LibProcess2/IExternalApplication.cs
@@ -0,0 +1,18 @@
+﻿namespace LibProcess2;
+
+public interface IExternalApplication
+{
+    Task<int> Run(
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    );
+
+    Task<(int exitCode, string stdOut, string stdErr)> RunWithResult(
+        IEnumerable<string> arguments,
+        string? workingDirectory = null,
+        CancellationToken? cancellationToken = null,
+        Func<int, bool>? isSuccess = null
+    );
+}
